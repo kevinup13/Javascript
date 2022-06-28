f1d26cc69f1707785246a8f6d1c38b946bdb2c58
kevin lopes albuquerque <kevinup13@gmail.com>
2022-06-27 20:03:05 -0400

    atualuzacao de cogigo

--- projeto1/assets/scripts/script.js
+++ projeto1/assets/scripts/script.js
@@ -1,1 +1,14 @@
+const hora = new Date;
+hora.getHours(hora);
+
+alert(hora);
+/*
+if (hora < 12){
+    alert("Bom dia!");
+}else if (hora < 18){
+    alert("Boa tarde!");
+}else if (hora <= 23){
+    alert("Boa Noite!");
+}
+*/
 alert("Bem vindo(a) ao Dominando Javascript!");
