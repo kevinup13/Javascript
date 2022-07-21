5e773c8c542d9d4f05b11e4b735b58f6862a3296
kevin lopes albuquerque <kevinup13@gmail.com>
2022-07-21 00:00:53 -0400

    novas modificacoes no arquivo js

--- projeto1/assets/scripts/script.js
+++ projeto1/assets/scripts/script.js
@@ -1,23 +1,18 @@
-const hora = new Date;
-hora.getHours(hora);
 
-alert(hora);
-/*
-if (hora < 12){
-    alert("Bom dia!");
-}else if (hora < 18){
-    alert("Boa tarde!");
-}else if (hora <= 23){
-    alert("Boa Noite!");
-}
-*/
-alert("Bem vindo(a) ao Dominando Javascript!");
 function azul() {
-    document.getElementById("")
+    limpar();
+    document.getElementById("titulo").classList.add('azul');
 }
 function vermelho(){
-
+    limpar();
+    document.getElementById("titulo").classList.add('vermelho');
 }
 function verde(){
-
+    limpar();
+    document.getElementById("titulo").classList.add('verde');
+}
+function limpar(){
+    document.getElementById("titulo").classList.remove('azul');
+    document.getElementById("titulo").classList.remove('vermelho');
+    document.getElementById("titulo").classList.remove('verde');
 }
