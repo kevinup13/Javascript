dc535b21cf2ff636dbe1fca67433fbe5fb778f83
kevin lopes albuquerque <kevinup13@gmail.com>
2022-07-25 19:17:17 -0400

    inserindo foor loop no js

--- projeto1/assets/scripts/script.js
+++ projeto1/assets/scripts/script.js
@@ -20,3 +20,21 @@ function limpar(){
     document.getElementById("titulo").classList.remove('verde');
     document.getElementById("titulo").classList.remove('limpar');
 }
+// FOR LOOP
+// FOR LOOP ARRAY
+
+let texto = '';
+for(let i = 1; i <= 50; i++) {
+    texto = texto + i + ", ";
+} 
+
+document.getElementById("demo-1").innerHTML = texto;
+
+let carros = ['Ferrari', 'fusca', 'palio', 'corola'];
+let html ='<ul>';
+
+for(let i in carros){
+    html += '<li>'+ carros[i] +'</li>';
+}
+html += '<ul>';
+document.getElementById("demo-2").innerHTML = html;
