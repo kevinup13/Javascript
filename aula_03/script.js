3fa5341e4d61af1bda048424327c669b4d7b619e
kevin lopes albuquerque <kevinup13@gmail.com>
2022-08-09 15:52:14 -0400

    Add Buttons

--- aula_03/script.js
+++ aula_03/script.js
@@ -1,2 +1,22 @@
-document.getElementById('text').innerHTML = "Javascript";
-document.getElementsByClassName('container').style.display:flex;
+
+function verde (){
+    limparVerde();
+    document.querySelector('#containerVerde').classList.add('verde')    
+};
+function vermelho (){
+    limparVermelho();
+    document.querySelector('#containerVermelho').classList.add('vermelho')   
+};
+function azul (){
+    limparAzul();
+    document.querySelector('#containerAzul').classList.add('azul')   
+};
+function limparVerde(){
+    document.querySelector("#containerVerde").classList.remove('verde');  
+}
+function limparVermelho(){
+    document.querySelector("#containerVermelho").classList.remove('vermelho');   
+}
+function limparAzul(){
+    document.querySelector("#containerAzul").classList.remove('azul');
+}
