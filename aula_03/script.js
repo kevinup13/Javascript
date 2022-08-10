3c599a1a6d0a7ed5fb91a1a906964f613bf902e3
kevin lopes albuquerque <kevinup13@gmail.com>
2022-08-10 14:17:40 -0400

    alteracao basica no js

--- aula_03/script.js
+++ aula_03/script.js
@@ -1,26 +1,42 @@
 
 function verde (){
     limparVerde();
-    document.querySelector('#containerVerde').classList.add('verde')    
+    lineUm();
+    document.querySelector('#containerVerde').classList.add('verde');
 };
 function vermelho (){
     limparVermelho();
-    document.querySelector('#containerVermelho').classList.add('vermelho')   
+    lineDois();
+    document.querySelector('#containerVermelho').classList.add('vermelho');   
 };
 function azul (){
     limparAzul();
-    document.querySelector('#containerAzul').classList.add('azul')   
+    lineTres();
+    document.querySelector('#containerAzul').classList.add('azul');   
 };
 function limparVerde(){
-    document.querySelector("#containerVerde").classList.remove('verde');  
+    document.querySelector("#containerVerde").classList.remove('verde');
+    document.querySelector(".lineUm").classList.remove("line-01") 
 };
 function limparVermelho(){
-    document.querySelector("#containerVermelho").classList.remove('vermelho');   
+    document.querySelector("#containerVermelho").classList.remove('vermelho');
+    document.querySelector(".lineDois").classList.remove("line-02")   
 };
 function limparAzul(){
     document.querySelector("#containerAzul").classList.remove('azul');
+    document.querySelector(".lineTres").classList.remove("line-03")
 }
 /* Essa função faz a troca de imagens através do atributo */
 function trocarImagem(filename){
     document.querySelector('.imagem').setAttribute('src', filename);
+}
+/* essa função muda a cor da line */
+function lineUm(){
+    document.querySelector(".lineUm").classList.add("line-01");
+}
+function lineDois(){
+    document.querySelector(".lineDois").classList.add("line-02");
+}
+function lineTres(){
+    document.querySelector(".lineTres").classList.add("line-03");
 }
