2797e5309888bc28f1bd0ef8814fd61b5e412174
kevin lopes albuquerque <kevinup13@gmail.com>
2022-10-10 23:31:27 -0400

    Novo projeto

---
+++ aula-04/script.js
@@ -0,0 +1,15 @@
+function square(x) {
+    return x * x;
+
+}
+
+function addSquare(a, b) {
+    let sqrA = square(a);
+    let sqrB = square(b);
+    return sqrA + sqrB;
+
+}
+console.log(addSquare(2, 3));
+
+
+console.log("Hello World!!");
