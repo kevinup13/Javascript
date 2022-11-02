32d2d7f8628c6f4dadb093272c7a7b95bc0d6044
kevin lopes albuquerque <kevinup13@gmail.com>
2022-11-02 13:17:58 -0400

    projeto danki

--- aula-04/script.js
+++ aula-04/script.js
@@ -1,15 +1,21 @@
-function square(x) {
-    return x * x;
+/* let carros = ['BW', 'Ferrari', 'Mercedes'];
+let x = 1;
+console.log('1. ' + carros[x]);
 
+let carros = ['BW', 'Ferrari', 'Mercedes'];
+carros[1] = 'Audi';
+let x = 1;
+console.log('1. ' + carros[x]);
+
+let carros = ['BW', 'Ferrari', 'Mercedes'];
+carros.push('Volvo');
+console.log('Itens no array: ');
+console.log(carros.length); */
+
+let personagem = {
+    nome: 'kevin',
+    idade: 28,
+    pais: 'Brasil',
+    forca: 100,
 }
-
-function addSquare(a, b) {
-    let sqrA = square(a);
-    let sqrB = square(b);
-    return sqrA + sqrB;
-
-}
-console.log(addSquare(2, 3));
-
-
-
+console.log(`${personagem.nome} tem ${personagem.forca} de força.`);
