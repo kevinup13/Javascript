100831f8569608974e9dcfc2b9eca8beada50433
kevin lopes albuquerque <kevinup13@gmail.com>
2022-11-17 16:00:44 -0400

    alteracao de arquivos

--- aula-04/script.js
+++ aula-04/script.js
@@ -12,10 +12,35 @@ carros.push('Volvo');
 console.log('Itens no array: ');
 console.log(carros.length); */
 
-let personagem = {
+/* let personagem = {
     nome: 'kevin',
     idade: 28,
     pais: 'Brasil',
     forca: 100,
 }
 console.log(`${personagem.nome} tem ${personagem.forca} de força.`);
+ */
+
+
+/* let cores = ['preto', 'branco', 'azul', 'vermelho',];
+ */
+
+/* for (let n = 0; n < cores.length; n++) {
+    console.log(cores[n]);
+} */
+
+
+let cores = [
+    { nome: 'preto', qt: 10 },
+    { nome: 'branco', qt: 5 },
+    { nome: 'azul', qt: 6 },
+    { nome: 'vermelho', qt: 20 }
+];
+for (let i in cores) {
+    cores[i].nome = cores[i].nome.toUpperCase();
+    console.log(cores[i]);
+};
+
+/* for (let cor of cores) {
+    console.log(`Nome: ${cor.nome} - ${cor.qt}`);
+} */
